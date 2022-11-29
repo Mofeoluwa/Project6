f06e4a693b504edf0070a720d8a509fe516e5842
Mofeoluwa Jide-Jegede <42873366+Mofeoluwa@users.noreply.github.com>
2022-11-29 01:42:21 -0500

    Add files via upload

---
+++ LibraryKiosk/LibraryKiosk/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace LibraryKiosk
+{
+    public class Book: IComparable<Book>
+
+    {
+
+   
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Pages { get; set; }
+
+        public string Publisher { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine();
+        }
+
+        public int CompareTo(Book? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
---
+++ LibraryKiosk/LibraryKiosk/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace LibraryKiosk;
+
+class Program
+{
+
+public static void Main(string[] args)
+    {
+        string path = @"books.csv";
+
+        //string lineFromCSV:= "";
+       
+
+        using (StreamReader sr = new StreamReader(path))
+        {
+            AvlTree tree = new AvlTree();
+            int i = 1;
+            string line;
+            List<string> lines;
+            Node node = new Node();
+            while ((line = sr.ReadLine()) != null)
+            {
+                lines = ProcessCSVLine(line);
+                Book book = new Book();
+                book.Title = lines[0];
+                book.Author = lines[1];
+                book.Pages = Int32.Parse(lines[2]);
+                book.Publisher = lines[3];
+                i++;
+
+                //Console.WriteLine(book.Title);
+                //Console.WriteLine(book.Author);
+                //Console.WriteLine(book.Pages);
+                //Console.WriteLine(book.Publisher);
+
+                book.Print();
+                node.book = book;
+                node.Title = lines[0];
+
+
+
+            }
+
+
+        };
+
+
+
+        List<string> ProcessCSVLine(string lineFromCSV)
+        {
+
+            string[] rawBookParts = lineFromCSV.Split(",");
+            List<string> sanitizedBookParts = new List<string>();
+            string cleanString = string.Empty;
+            for (int i = 0; i < rawBookParts.Length; i++)
+            {
+                cleanString += rawBookParts[i];
+                if (cleanString.StartsWith("\"") && !cleanString.EndsWith("\""))
+                {
+                    continue;
+                }
+                sanitizedBookParts.Add(cleanString.Replace("\"", String.Empty));
+                cleanString = String.Empty;
+
+            }
+
+            return sanitizedBookParts;
+
+
+        }
+
+
+
+        //tree.Add(5);
+        //tree.DisplayTree();
+
+
+    } // End Main``` }
+}
+
